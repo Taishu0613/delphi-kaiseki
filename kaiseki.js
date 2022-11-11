d889766e79d9b884f9d259402a8d5a5d945814ff
taishu0613 <taishu_wakasugi@mjs.co.jp>
2022-11-11 13:16:33 +0900

    とりあえずバグだらけ

--- kaiseki.js
+++ kaiseki.js
@@ -5,11 +5,12 @@ var str;
 var Bunseki_element = document.getElementById("Result_element");
 var b = 0;
 var a = 0;
+var Mode = "なし";//”explain”：説明文 "content":コンテンツ
+var strsplit=[];
 var CodeInfo = {};
 var CodeAllInfo = [];
 var cut1       = [];//コード内容と、説明文に分ける
-var ExplainB   = "";//説明文内容
-var Expalin    = [];//説明文達の配列
+var Explain    = [];//説明文達の配列
 var ContentB   = "";//コンテンツ内容
 var Content    = [];//コンテンツ達の配列
 var LineCnt = [];
@@ -17,6 +18,8 @@ var AllLineCnt = 0;
 var Comment_;
 var Surash;
 var CommentCreate;
+var SENGENcheck;
+var Comment_Boolean;
 
 //ダイアログでファイルが選択された時
 obj1.addEventListener(
@@ -32,60 +35,90 @@ obj1.addEventListener(
             BunsekiString.value = reader.result;
             str = BunsekiString.value;
             Bunkai(str);
+            
         }
     },
     false);
 
 function Bunkai(str) {
-
-    cut1 = str.split("************************************************************************\n");
-
-    for (var i = 0; i < cut1.length - 1; i++) {
-        if (i % 2 == 0) {
-            Content[a] = cut1[i+2];
-            
-            ContentB = Content[a];
-            ContentBunseki(ContentB);
-            a++;
-        }
-        else if (i % 2 == 1) {
-            Expalin[b] = cut1[i];
-            Expalin[b] = Expalin[b].replace(/[//]/g, '');
-            Expalin[b] = Expalin[b].replace(/[//*]/g, '');
-            Expalin[b] = Expalin[b].replace(/[//**]/g, '');
-
-            //console.log(b, "個目");
-            if(b === 0){
-                console.log(b)
-                HenkouLog(Expalin[0])
+    var ModeGNo=0;
+    strsplit=str.split("\n");//全てのコードを改行で配列に組み込む
+    for(var i = 0; i < strsplit.length; i++){
+        console.log(i);
+        CodeInfo.Line=i;//コードの行数を記録
+        CodeInfo.String=strsplit[i].trim();//コードの文字列を記録
+        CodeInfo.ModeGNo=ModeGNo;//何個目の説明文とコンテンツかを記録
+//----------------------------------------説明文とコンテンツ切替処理--------------------------------------------------------------------------------
+        if(CodeInfo.String.includes("************************************************************************")===true)//説明文とコンテンツが切り替わる文字列が含まれているかどうか
+        {   
+            if(Mode==="なし")//Modeの初期値は”なし”
+            Mode="説明文";//説明文モード   
+            else if(Mode==="コンテンツ") 
+            {
+                Mode="説明文";//説明文モード
+                ModeGNo++;//説明文モードに切り替わった場合、モードグループを１上げる
             }
-            ExplainB = Expalin[b];
-            Bunseki(ExplainB);
-            b++;
+            else if(Mode==="説明文")
+            {
+                Mode="コンテンツ";//コンテンツモード
+            }
+            CodeInfo.ModeContentLine=0;//コンテンツモードの何行目かを初期化
         }
+//----------------------------------------説明文とコンテンツ切替処理終了--------------------------------------------------------------------------------
+
+        else if(Mode=="コンテンツ")//コンテンツモードの場合
+        {
+            CodeInfo.Mode="コンテンツ"//モード：コンテンツと記録する     
+            ContentBunseki();
+            a++;
+            CodeInfo.ModeContentLine++;//コンテンツモードの何行目かを記録
+        }
+        else if(Mode=="説明文")//説明文モードの場合
+        {
+            CodeInfo.Mode="説明文"//モード：説明文と記録する
+            CodeInfo.String=CodeInfo.String.replace(/[//]/g, '');
+            CodeInfo.String=CodeInfo.String.replace(/[//*]/g, '');
+            CodeInfo.String=CodeInfo.String.replace(/[//**]/g, '');
+            CodeInfo.String=CodeInfo.String.trim();
+            if(Explain[ModeGNo]==undefined)Explain[ModeGNo]="";
+            Explain[ModeGNo] = Explain[ModeGNo] + CodeInfo.String + "\n";//説明文の塊を作成する
+        }
+        
+        CodeAllInfo[i]=CodeInfo;
     }
+    console.log(Explain[0]);
+    console.log("CodeAllInfo完了");
+    for (var i = 0; i < ModeGNo; i++) {
+        if(i === 0){
+            console.log(ModeGNo);
+            HenkouLog(Explain[0]);
+        }
+        Bunseki(Explain[i],i);
+    }
+
 };
+
 //*****************************************************************************************************************
 //コンテンツを分析
 //***************************************************************************************************************** 
-function ContentBunseki(ContentB) {
+function ContentBunseki() {
     var ContentChild = [];
-    ContentChild = ContentB.split("\n");
+    
+    ContentChild = CodeInfo.String;
     //最初のコンテンツのみ、宣言分析関数を使う。
-    if (a == 0)
-        SengenBunseki(ContentChild);
+    if (CodeInfo.ModeGNo == 0)
+        SengenBunseki();
     LineCnt[a] = ContentChild.length - 1;
-
 }
 
 //-----------------コンテンツのコードとコメントを区別する関数-----------------------------------------------------------
-function CommentOutBunri(i, ContentChild) {
+function CommentOutBunri() {
 
     //[i]番目の行にコメントアウトがある場合、コメントアウト部分のみ抜き出す。
-    if (ContentChild[i].includes('//')) {
-        Surash = ContentChild[i].indexOf('//'); //ダブルスラッシュの位置の数字を抜き出す
-        Comment_ = ContentChild[i].substring(Surash + 2);//ダブルスラッシュ以降の文字を抽出
-        ContentChild[i] = ContentChild[i].replace('//' + Comment_, "");//コメントを文字列から削除
+    if (CodeInfo.String.includes('//')) {
+        Surash = CodeInfo.String.indexOf('//'); //ダブルスラッシュの位置の数字を抜き出す
+        Comment_ = CodeInfo.String.substring(Surash + 2);//ダブルスラッシュ以降の文字を抽出
+        CodeInfo.String = CodeInfo.String.replace('//' + Comment_, "");//コメントを文字列から削除
         CommentCreate = document.createElement('span');//div要素作成
         CommentCreate.className = 'Comment_list';//Classを指定
         CommentCreate.textContent = Comment_;//表示文字列を指定
@@ -93,79 +126,77 @@ function CommentOutBunri(i, ContentChild) {
     }
 }
 //-------------------------------------宣言部分を分類する関数------------------------------------------------------------
-function SengenBunseki(ContentChild) {
-    var SENGENcheck;
-    var Comment_Boolean;
-    for (var i = 0; i < ContentChild.length; i++)//一行ずつ特定の言葉を含んでいないか検査
-    {
-        ContentChild[i] = ContentChild[i].trim();
-        if (ContentChild[i].includes('//')) {
-            CommentOutBunri(i, ContentChild);
-            Comment_Boolean = true;
-        }
-        //[i]番目の行　検査開始
-        if (ContentChild[i].toUpperCase().startsWith('CONST') === true) {
-            SENGENcheck = "Const";
-        }
-        else if (ContentChild[i].toUpperCase().startsWith('TYPE') === true) {
-            SENGENcheck = "Type";
-        }
-        else if (ContentChild[i].toUpperCase().startsWith('PUBLIC') === true) {
-            SENGENcheck = "Public";
-        }
-        else if (ContentChild[i].toUpperCase().startsWith('PRIVATE') === true) {
-            SENGENcheck = "Private";
-        }
-        else if (ContentChild[i].toUpperCase().startsWith('FUNCTION') === true) {
-            SENGENcheck = "Function";
-        }
-        else if (ContentChild[i].toUpperCase().startsWith('PROCEDURE') === true) {
-            SENGENcheck = "Procedure";
-        }
-        //[i]番目の行　検査終了
-
-        //続いてHTMLに配置
-        var Sengen_listCreate = document.createElement('li');//li要素作成
-        Sengen_listCreate.textContent = ContentChild[i];//表示文字列を指定
-
-        //続いてHTMLに配置
-        if (SENGENcheck === "Const") {
-            Const_list.appendChild(Sengen_listCreate);//配置場所を指定
-        }
-        if (SENGENcheck === "Type") {
-            Type_list.appendChild(Sengen_listCreate);//配置場所を指定
-        }
-        if (SENGENcheck === "Public") {
-            Public_list.appendChild(Sengen_listCreate);//配置場所を指定
-        }
-        if (SENGENcheck === "Private") {
-            Private_list.appendChild(Sengen_listCreate);//配置場所を指定
-        }
-        if (SENGENcheck === "Function") {
-            Function_list.appendChild(Sengen_listCreate);//配置場所を指定
-        }
-        if (SENGENcheck === "Procedure") {
-            Procedure_list.appendChild(Sengen_listCreate);//配置場所を指定
-        }
-        //コメントが含まれている場合のみ配置
-
-        if (Comment_Boolean) {
-            Sengen_listCreate.appendChild(CommentCreate);//配置場所を指定
-            Comment_Boolean = false;
-        }
-
+function SengenBunseki() {
+    //一行ずつ特定の言葉を含んでいないか検査
+    
+    CodeInfo.String = CodeInfo.String.trim();
+    if (CodeInfo.String.includes('//')) {
+        CommentOutBunri();
+        Comment_Boolean = true;
     }
+    //[i]番目の行　検査開始
+    if (CodeInfo.String.toUpperCase().startsWith('CONST') === true) {
+        SENGENcheck = "Const";
+    }
+    else if (CodeInfo.String.toUpperCase().startsWith('TYPE') === true) {
+        SENGENcheck = "Type";
+    }
+    else if (CodeInfo.String.toUpperCase().startsWith('PUBLIC') === true) {
+        SENGENcheck = "Public";
+    }
+    else if (CodeInfo.String.toUpperCase().startsWith('PRIVATE') === true) {
+        SENGENcheck = "Private";
+    }
+    else if (CodeInfo.String.toUpperCase().startsWith('FUNCTION') === true) {
+        SENGENcheck = "Function";
+    }
+    else if (CodeInfo.String.toUpperCase().startsWith('PROCEDURE') === true) {
+        SENGENcheck = "Procedure";
+    }
+    //[i]番目の行　検査終了
+
+    //続いてHTMLに配置
+    var Sengen_listCreate = document.createElement('li');//li要素作成
+    Sengen_listCreate.textContent =CodeInfo.String;//表示文字列を指定
+
+    //続いてHTMLに配置
+    if (SENGENcheck === "Const") {
+        Const_list.appendChild(Sengen_listCreate);//配置場所を指定
+    }
+    if (SENGENcheck === "Type") {
+        Type_list.appendChild(Sengen_listCreate);//配置場所を指定
+    }
+    if (SENGENcheck === "Public") {
+        Public_list.appendChild(Sengen_listCreate);//配置場所を指定
+    }
+    if (SENGENcheck === "Private") {
+        Private_list.appendChild(Sengen_listCreate);//配置場所を指定
+    }
+    if (SENGENcheck === "Function") {
+        Function_list.appendChild(Sengen_listCreate);//配置場所を指定
+    }
+    if (SENGENcheck === "Procedure") {
+        Procedure_list.appendChild(Sengen_listCreate);//配置場所を指定
+    }
+    //コメントが含まれている場合のみ配置
+
+    if (Comment_Boolean) {
+        Sengen_listCreate.appendChild(CommentCreate);//配置場所を指定
+        Comment_Boolean = false;
+    }
+
+    
 
 }
 
 //*****************************************************************************************************************
 //説明文を分析
 //***************************************************************************************************************** 
-function Bunseki(ExplainB) {
+function Bunseki(Explain,i) {
     var ExplainChild = [];
 
-    ExplainChild = ExplainB.split("\n");
-    ExplainChild[0] = ExplainChild[0].trim();
+    ExplainChild = Explain.split("\n");
+    // ExplainChild[0] = ExplainChild[0].trim();
 
 
     //------------------------------------------------------------HTMLに表示領域（ブロック）を作成------------------------------------------------------------    
@@ -176,7 +207,7 @@ function Bunseki(ExplainB) {
     //-------チェックボックス作成------------
     var CheckboxCreate = document.createElement('input');//li要素作成
     CheckboxCreate.type = 'checkbox';//typeを指定
-    CheckboxCreate.id = b + "title";//IDを指定
+    CheckboxCreate.id = i + "title";//IDを指定
     BLOCKCreate.appendChild(CheckboxCreate);//配置場所を指定
     //-------＋印作成------------
     var PlusCreate = document.createElement('div');//div要素作成
@@ -185,9 +216,10 @@ function Bunseki(ExplainB) {
     BLOCKCreate.appendChild(PlusCreate);//配置場所を指定
     //-------見出し作成------------
     var TitleLabel = document.createElement('label');//label要素作成
-    TitleLabel.htmlFor = b + "title";//Forを指定
+    TitleLabel.htmlFor = i + "title";//Forを指定
     TitleLabel.textContent = ExplainChild[0] + " " +  AllLineCnt + " " + LineCnt[b];//表示文字列を指定
-    AllLineCnt = AllLineCnt + (ExplainChild.length + LineCnt[b] + 1);
+    const ABCZ = CodeAllInfo.find((Line) => { return Line.ModeGNo===i });
+    console.log(ABCZ);
     BLOCKCreate.appendChild(TitleLabel);//配置場所を指定
     //-------コンテンツ作成------------
     var BLOCK_ContentCreate = document.createElement('label');//label要素作成
