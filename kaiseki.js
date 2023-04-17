83921e739cbf2535607a4434efa9e522f4bb9ddf
taishu0613 <taishu_wakasugi@mjs.co.jp>
2023-04-17 09:46:55 +0900

    使わない部分削除してみた

--- kaiseki.js
+++ kaiseki.js
@@ -103,7 +103,7 @@ function Bunkai(str) {
         Explains[i] = CodeInfoArray.filter((CodeInfo) => CodeInfo.Mode === "説明文" && CodeInfo.BlockNo === i);
         //コンテンツの塊を作成する
         Contents[i] = CodeInfoArray.filter((CodeInfo) => CodeInfo.Mode === "コンテンツ" && CodeInfo.BlockNo === i);
-        ExplainBunseki(Explains[i], i);//説明文を分析
+        //ExplainBunseki(Explains[i], i);//説明文を分析
         ContentBunseki(Contents[i], i);//コンテンツを分析
     }
     console.log("ブロックの数=", BlockNo);
@@ -120,95 +120,6 @@ function Bunkai(str) {
     console.log(CodeInfoArray);
 };
 
-
-//*****************************************************************************************************************
-//説明文を分析
-//***************************************************************************************************************** 
-function ExplainBunseki(Explain, i) {
-    //------------------------------------------------------------HTMLに表示領域（ブロック）を作成------------------------------------------------------------    
-    //-------タブブロック、div要素を作成------------
-    let BLOCKCreate = document.createElement('div');//div要素作成
-    BLOCKCreate.className = 'BLOCK';//クラスを指定
-    category2.appendChild(BLOCKCreate);//配置場所を指定
-    //-------チェックボックス作成------------
-    let CheckboxCreate = document.createElement('input');//li要素作成
-    CheckboxCreate.type = 'checkbox';//typeを指定
-    CheckboxCreate.id = i + "title";//IDを指定
-    BLOCKCreate.appendChild(CheckboxCreate);//配置場所を指定
-    //-------＋印作成------------
-    let PlusCreate = document.createElement('div');//div要素作成
-    PlusCreate.className = 'cp_plus';//Classを指定
-    PlusCreate.textContent = "+";//表示文字列を指定
-    BLOCKCreate.appendChild(PlusCreate);//配置場所を指定
-    //-------見出し作成------------
-    let TitleLabel = document.createElement('label');//label要素作成
-    TitleLabel.htmlFor = i + "title";//Forを指定
-    const Code = CodeInfoArray.find((CodeInfo) => { return CodeInfo.BlockNo === i && CodeInfo.Mode === 'コンテンツ' });
-    if (i === 0)
-        TitleLabel.textContent = Explain[1].String + " " + Code.Line + " " + Contents[i].length;//表示文字列を指定
-    else
-        TitleLabel.textContent = Explain[0].String + " " + Code.Line + " " + Contents[i].length;//表示文字列を指定
-
-    BLOCKCreate.appendChild(TitleLabel);//配置場所を指定
-    //-------コンテンツ作成------------
-    let BLOCK_ContentCreate = document.createElement('label');//label要素作成
-    BLOCK_ContentCreate.className = 'BLOCK-content';//Classを指定
-    BLOCKCreate.appendChild(BLOCK_ContentCreate);//配置場所を指定
-    for (let i = 1; i < Explain.length - 1; i++) {
-        //Explain[i].String = Explain[i].String.replace("//", '');
-        let BLOCK_Content_SplitCreate = document.createElement('li');//li要素作成
-        BLOCK_Content_SplitCreate.textContent = Explain[i].String;//表示文字列を指定
-        BLOCK_ContentCreate.appendChild(BLOCK_Content_SplitCreate);//配置場所を指定
-    }
-
-    //-------------------------------------コンポーネントとプロセスとその他に分類する------------------------------------------------------------
-
-    if (Explain[0].String.includes('Proccess') === true) {
-        BLOCKCreate.className = BLOCKCreate.className + ' Proccess_list';//クラスを指定
-        //console.log("プロセス");
-    }
-    else if (Explain[0].String.includes('Component') === true) {
-        BLOCKCreate.className = BLOCKCreate.className + ' Component_list';//クラスを指定
-        //console.log("コンポネート");
-    }
-    else {
-        BLOCKCreate.className = BLOCKCreate.className + ' Sonota_list';//クラスを指定 
-    }
-
-};
-
-//-------------------------------------------------ソート--------------------------------------------------
-//document.getElementById("sortC").addEventListener("change",aaa());
-function SortCode() {
-    let checkedC = document.getElementById("sortCompornent").checked;
-    let checkedP = document.getElementById("sortProcess").checked;
-    let checkedO = document.getElementById("sortOther").checked;
-    let checked = document.getElementById("sortAll").checked;
-    let aa = document.querySelectorAll(".Proccess_list, .Component_list, .Sonota_list");
-    let cc = document.querySelectorAll(".Proccess_list, .Sonota_list");
-    let pp = document.querySelectorAll(".Component_list, .Sonota_list");
-    let oo = document.querySelectorAll(".Proccess_list, .Component_list");
-    //if(checked){
-    for (let i = 0; i < aa.length; i++) {
-        aa[i].style.display = "block";
-    }
-    //}
-    if (checkedC == true) {
-        for (let i = 0; i < cc.length; i++) {
-            cc[i].style.display = "none";
-        }
-    }
-    if (checkedP == true) {
-        for (let i = 0; i < pp.length; i++) {
-            pp[i].style.display = "none";
-        }
-    }
-    if (checkedO == true) {
-        for (let i = 0; i < oo.length; i++) {
-            oo[i].style.display = "none";
-        }
-    }
-}
 //--------------------------変更履歴コンボボックス作成--------------------------
 function HenkouLog(SystemExplain) {
     let L = 0;
@@ -242,44 +153,7 @@ function HenkouLog(SystemExplain) {
     }
     console.log("変更履歴格納完了");
 }
-function HenkouKensaku(LogID) {
-    let LogIDPoint = 0;
-    for (let a = 0; a < Contents.length; a++) {//コンテンツを一つずつ調べる
-        let Content = Contents[a];
-        let BeginEnd = false;
-        for (let b = 0; b < Content.length; b++) {//コンテンツの行を一つずつ調べる
-            let ContentChild = Content[b];
-            if (ContentChild.String.includes(LogID) && ContentChild.String.startsWith("//")) {//コメントアウトでログIDが見つかった場合
-                let DelREIGAI = (ContentChild.String.match(/\/\//g) || []).length;
-                //　文字列に"//"が２つ以上のときはDELと認識し、一行だけ抜き出す。
-                if (DelREIGAI === 1) {//DelREIGAIではない時（通常の処理）
-                    if (BeginEnd) {
-                        ContentChild.LogID = LogID;
-                        ContentChild.LogIDPoint = LogIDPoint;
-                    }
-                    BeginEnd = !BeginEnd;//判定反転
-                    if (BeginEnd === false) {
-                        LogIDPoint++;
-                    }
-                }
-                else {//DelREIGAIの時
-                    ContentChild.LogID = LogID;
-                    ContentChild.LogIDPoint = LogIDPoint;
-                    LogIDPoint++;
-                }
-            }
-            else if (ContentChild.String.includes(LogID)) {//ログＩDが文字の最初ではないときは1行だけ抜き出す。
-                ContentChild.LogID = LogID;
-                ContentChild.LogIDPoint = LogIDPoint;
-                LogIDPoint++;
-            }
-            if (BeginEnd) {
-                ContentChild.LogID = LogID;
-                ContentChild.LogIDPoint = LogIDPoint;
-            }
-        }
-    }
-}
+
 function HenkouKensakuVer2(LogID,UnitNo) {
     let LogIDPoint = 0;
     let BeginEnd = false;
